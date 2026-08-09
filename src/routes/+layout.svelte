78d65256029b7fc1fc8f1f8ecbb81a9b72ebc0cf
Rogel <rogelprogrammer@gmail.com>
2026-08-09 09:50:34 -0400

    implemented vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { inject } from '@vercel/analytics';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		inject();
+	});
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
